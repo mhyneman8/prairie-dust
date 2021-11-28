03b97c0fc6f2b8235db022895c628bc6e8f0a8a0
mhyneman8 <mallory.hyneman@gmail.com>
2021-11-28 14:46:13 -0600

    install dependencies

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import MainView from './components/main-view/main-view';
+
 function App() {
   return (
     <div className="App">
---
+++ src/components/MainView/main-view.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class MainView extends React.Component {
+    render() {
+        return (
+            <div className="main-view">
+                <div>Soap 1</div>
+                <div>Soap 2</div>
+                <div>Soap 3</div>
+            </div>  
+        );
+    }
+}
+
+export default MainView;
