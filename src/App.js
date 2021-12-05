4742fb04038650cbd2164d019504713960486ff4
mhyneman8 <mallory.hyneman@gmail.com>
2021-12-05 16:50:39 -0600

    change class to functional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 import StoreView from './components/store-view/store-view';
+import Navbar from './components/navbar/navbar';
+// import AboutView from './components/about-view/about-view';
+// import { Router, Route } from 'react-router-dom';
+// import StoreView from './components/store-view/store-view';
 
-class App extends Component {
-  render() {
+function App() {
+  // const [itemCount, setItemCount] = useState(0);
     return (
-      <StoreView />
-      
+      <React.Fragment>
+        <Navbar />
+        <StoreView />
+      </React.Fragment>
+        
     );
-  }
 }
+
 export default App;
