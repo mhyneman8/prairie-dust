a452c0c13a67e7202cbe3b47b48f768655c1a0dc
mhyneman8 <mallory.hyneman@gmail.com>
2021-12-05 16:50:01 -0600

    navbar bootstrap

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,16 +3,22 @@ import { NavLink } from 'react-router-dom';
 
 function Navbar() {
         return (
-            <div className="nav-view">
-                <div className="container">
-                    <h1>Prairie Dust</h1>
+            <nav className=" navbar navbar-expand-lg nav-view">
+                <div className="container-fluid">
+                    <h1 className="navbar-brand">Prairie Dust</h1>
                     <h3>Homemade Soap</h3> 
+                    
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarNav">
 
                     <ul className="navbar-nav">
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/">
                                 Home
-                                <span className="sr-only">(current)</span>
+                                {/* <span className="sr-only">(current)</span> */}
                             </NavLink> 
                         </li>
                         <li className="nav-item">
@@ -27,12 +33,13 @@ function Navbar() {
                         </li>
                         <li className="nav-item">
                             <NavLink className="nav-link" to="/cart">
-                                Cart
+                                {/* Cart {itemCount} */}
                             </NavLink>
                         </li>
                     </ul>
+                    </div>
                 </div>
-            </div>
+            </nav>
         );
     }
 
