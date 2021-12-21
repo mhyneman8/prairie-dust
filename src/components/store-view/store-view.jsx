0ebb4894b703efb94d12a56b8a845666a84c0b48
mhyneman8 <mallory.hyneman@gmail.com>
2021-12-21 10:52:23 -0600

    update store-view

--- src/components/store-view/store-view.jsx
+++ src/components/store-view/store-view.jsx
@@ -1,90 +1,86 @@
-import React, { Component } from 'react';
-import products from '../../products.json';
+import React, { useContext } from 'react';
+import products from '../../products';
 // import Cart from '../cart/cart';
-import Navbar from '../navbar/navbar';
+// import Navbar from '../navbar/navbar';
 import SoapView from '../soap-view/soap-view';
 import SoapCard from '../soap-card/soap-card';
 import './store-view.css';
+import CartContext from '../../context/cart/CartContext';
 
 
+// class StoreView extends Component {
+    // constructor(props) {
+    //     super(props);
+    //     this.removeFromCart = this.removeFromCart.bind(this);
+    //     this.state = { 
+    //         cart: [],
+    //         total: 0,
+    //         selectedSoap: null
+    //     }
+    // };
 
-class StoreView extends Component {
-    constructor(props) {
-        super(props);
-        this.removeFromCart = this.removeFromCart.bind(this);
-        this.state = { 
-            pageTitle: "Main",
-            customersCount: 5,
-            cart: [],
-            total: 0,
-            selectedSoap: null
-        }
-    };
+    // componentDidMount() {
+    //     const storedCart = localStorage.getItem('cart');
+    //     const storedTotal = localStorage.getItem('total');
+    //     if (storedCart !== null) {
+    //         const parsedCart = JSON.parse(storedCart);
+    //         this.setState({ cart: parsedCart });
+    //     }
+    //     if (storedTotal !== null) {
+    //         const parsedTotal = JSON.parse(storedTotal);
+    //         this.setState({ total: parsedTotal})
+    //     }
+    // }
 
-    componentDidMount() {
-        const storedCart = localStorage.getItem('cart');
-        const storedTotal = localStorage.getItem('total');
-        if (storedCart !== null) {
-            const parsedCart = JSON.parse(storedCart);
-            this.setState({ cart: parsedCart });
-        }
-        if (storedTotal !== null) {
-            const parsedTotal = JSON.parse(storedTotal);
-            this.setState({ total: parsedTotal})
-        }
-    }
+    // addToCart = (product) => {
+    //     const obj = {"image": product.image, "title": product.title, "price": product.price};
+    //     let newCart = this.state.cart;
+    //     newCart.push(obj);
+    //     this.setState( { cart: newCart });
+    //     const stringifiedCart = JSON.stringify(this.state.cart);
+    //     localStorage.setItem('cart', stringifiedCart);
 
-    addToCart = (product) => {
-        const obj = {"image": product.image, "title": product.title, "price": product.price};
-        let newCart = this.state.cart;
-        newCart.push(obj);
-        this.setState( { cart: newCart });
-        const stringifiedCart = JSON.stringify(this.state.cart);
-        localStorage.setItem('cart', stringifiedCart);
+    //     alert(`${product.title} added to cart!`);
+    //     this.updateTotal(product.price);
+    // }
 
-        alert(`${product.title} added to cart!`);
-        this.updateTotal(product.price);
-    }
+    // removeFromCart = (index) => {
+    //     let itemPrice = this.state.cart[index].price;
+    //     let newCart = [...this.state.cart];
 
-    removeFromCart = (index) => {
-        let itemPrice = this.state.cart[index].price;
-        let newCart = [...this.state.cart];
+    //     itemPrice = -Math.abs(itemPrice);
 
-        itemPrice = -Math.abs(itemPrice);
+    //     newCart.splice(index, 1);
+    //     this.setState({ cart: newCart });
 
-        newCart.splice(index, 1);
-        this.setState({ cart: newCart });
+    //     const stringifiedCart = JSON.stringify(newCart);
+    //     localStorage.setItem('cart', stringifiedCart);
 
-        const stringifiedCart = JSON.stringify(newCart);
-        localStorage.setItem('cart', stringifiedCart);
+    //     this.updateTotal(itemPrice)
+    //     alert(`${this.state.cart[index].title} removed from cart.`);
+    // }
 
-        this.updateTotal(itemPrice)
-        alert(`${this.state.cart[index].title} removed from cart.`);
-    }
+    // updateTotal = (price) => {
+    //     const newTotal = parseInt(this.state.total) + parseInt(price);
 
-    updateTotal = (price) => {
-        const newTotal = parseInt(this.state.total) + parseInt(price);
-
-        this.setState({ total: newTotal })
+    //     this.setState({ total: newTotal })
         
-        const stringifiedTotal = JSON.stringify(newTotal);
-        localStorage.setItem('total', stringifiedTotal);
-    }
+    //     const stringifiedTotal = JSON.stringify(newTotal);
+    //     localStorage.setItem('total', stringifiedTotal);
+    // }
 
     // setSelectedSoap(newSelectedSoap) {
     //     this.setState({
     //         selectedSoap: newSelectedSoap
     //     });
     // }
-
-    render() {
-        const { cart, total, selectedSoap } = this.state;
-
-        if (selectedSoap) return <SoapView product={selectedSoap} addToCart={this.addToCart} />;
+    const StoreView = () => {
+    // render() {
+        // const { cart, total, selectedSoap } = this.state;
+        
         
         return (
             <div className="main-view">
-            <Navbar cart={cart} total={total} updateTotal={this.updateTotal} removeFromCart={this.removeFromCart} />
             
             <div className="cover-photo">
                 <h3>BAR SOAP</h3>
@@ -93,33 +89,13 @@ class StoreView extends Component {
 
             <div className="grid">
             
-            {products.map(product => {
-                return (
-                    <div key={product.id} className="card">
-                        <img src={product.image} alt={`Preview of ${product.title}`} />
-                        <h3>{ product.title }</h3>
-                        {/* <p>{ product.description }</p>
-                        <p>{ product.size }</p> */}
-                        <p>${ product.price }</p>
-
-                        <button
-                            className="add-item"
-                            data-item-id={product.id}
-                            data-item-image={product.image}
-                            data-item-name={product.title}
-                            data-item-url="/"
-                            data-item-price={product.price}
-                            onClick={() => { this.addToCart(product)}}
-                        >  
-                            ADD TO CART
-                        </button>
-                    </div>
-                ); 
-            })} 
+                {products.map((product) => (
+                    <SoapCard key={product.id} product={product} />  
+                ))} 
             </div>
         </div>   
         );
     }
-}
+
 
 export default StoreView;
