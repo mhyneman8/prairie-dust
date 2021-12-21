39a099f374b6e0c6f4713600900aa266e643e108
mhyneman8 <mallory.hyneman@gmail.com>
2021-12-21 10:52:47 -0600

    add styling to soap-card

--- src/components/soap-card/soap-card.jsx
+++ src/components/soap-card/soap-card.jsx
@@ -1,33 +1,72 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import CartContext from '../../context/cart/CartContext';
+import SoapView from '../soap-view/soap-view';
+import './soap-card.css';
+import Modal from 'react-bootstrap/Modal'
 
-class SoapCard extends React.Component {
-    render() {
-        const { product, onSoapClick } = this.props;
+
+const SoapCard = ({ product }) => {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    // const handleShow = () => setShow(true);
+        const { addToCart, setShowMore, showMore } = useContext(CartContext);
         
-        return(
-            <div key={product.id} className="movie-card card">
-                <img src={product.image} alt={`Preview of ${product.title}`} />
-                <h3
-                    onClick={() => { onSoapClick(product); }}    
-                >{ product.title }</h3>
-                {/* <p>{ product.description }</p>
-                <p>{ product.size }</p> */}
-                <p>${ product.price }</p>
+        return (
+            <div>
+                {showMore ? 
 
-                <button
-                    className="add-item"
-                    data-item-id={product.id}
-                    data-item-image={product.image}
-                    data-item-name={product.title}
-                    data-item-url="/"
-                    data-item-price={product.price}
-                    onClick={() => { this.addToCart(product)}}
-                >  
-                    ADD TO CART
+                    <Modal.Dialog
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                    >
+                        <Modal.Header closeButton>
+                            <Modal.Title>{product.title}</Modal.Title>
+                        </Modal.Header>
+
+                        <Modal.Body>
+                            <SoapView key={product.id} product={product} /> 
+                        </Modal.Body>
+
+                        <Modal.Footer>
+                            <button variant="secondary" onClick={handleClose}>Close</button>
+                            <button className="add-item" 
+                                onClick={() => addToCart(product)}>
+                                Add to Cart
+                            </button>
+                        </Modal.Footer>
+                    </Modal.Dialog>
+
+                
+                : null }
+                <div className="soap-card__wrapper" >
+                
+                
+                <img className="soapCard__img" src={product.image}  alt="" />
+                <h4>{product.title}</h4>
+                <div className="productCard_price">
+                    <h5>{product.price}</h5>
+                </div>
+                <div>
+                    {showMore ? <div>
+                        <h6>{product.description}</h6>
+                        <h6>{product.size} oz</h6>
+                        <h6>Contains: </h6> <p>{ product.ingredients.map((ingredients) => ingredients + " ") }</p> 
+                        </div>
+                        : ""}
+                    <button className="show-more" onClick={() => setShowMore(product.id)}>
+                        {showMore ? "Show Less -" : "Show More +"}
+                    </button>
+                </div>
+                <button className="add-item" 
+                    onClick={() => addToCart(product)}>
+                    Add to Cart
                 </button>
-            </div>
+            </div> 
+        </div>
         ); 
     }
-}
 
 export default SoapCard
